fbd5cb3bc6f8eb27d1e3ab20d112cb21602c5466
WingLim <643089849@qq.com>
2020-11-16 20:05:12 +0800

    fix: change PYENV_HOME to PYENV_ROOT

--- zsh-pyenv.plugin.zsh
+++ zsh-pyenv.plugin.zsh
@@ -1,18 +1,18 @@
 GITHUB="https://github.com"
 
-[[ -z "$PYENV_HOME" ]] && export PYENV_HOME="$HOME/.pyenv"
+[[ -z "$PYENV_ROOT" ]] && export PYENV_ROOT="$HOME/.pyenv"
 
 # export PATH
-export PATH="$PYENV_HOME/bin:$PYENV_HOME/shims:$PATH"
+export PATH="$PYENV_ROOT/bin:$PYENV_ROOT/shims:$PATH"
 
 _zsh_pyenv_install() {
     echo "Installing pyenv..."
-    git clone "${GITHUB}/pyenv/pyenv.git"            "${PYENV_HOME}"
-    git clone "${GITHUB}/pyenv/pyenv-doctor.git"     "${PYENV_HOME}/plugins/pyenv-doctor"
-    git clone "${GITHUB}/pyenv/pyenv-installer.git"  "${PYENV_HOME}/plugins/pyenv-installer"
-    git clone "${GITHUB}/pyenv/pyenv-update.git"     "${PYENV_HOME}/plugins/pyenv-update"
-    git clone "${GITHUB}/pyenv/pyenv-virtualenv.git" "${PYENV_HOME}/plugins/pyenv-virtualenv"
-    git clone "${GITHUB}/pyenv/pyenv-which-ext.git"  "${PYENV_HOME}/plugins/pyenv-which-ext"
+    git clone "${GITHUB}/pyenv/pyenv.git"            "${PYENV_ROOT}"
+    git clone "${GITHUB}/pyenv/pyenv-doctor.git"     "${PYENV_ROOT}/plugins/pyenv-doctor"
+    git clone "${GITHUB}/pyenv/pyenv-installer.git"  "${PYENV_ROOT}/plugins/pyenv-installer"
+    git clone "${GITHUB}/pyenv/pyenv-update.git"     "${PYENV_ROOT}/plugins/pyenv-update"
+    git clone "${GITHUB}/pyenv/pyenv-virtualenv.git" "${PYENV_ROOT}/plugins/pyenv-virtualenv"
+    git clone "${GITHUB}/pyenv/pyenv-which-ext.git"  "${PYENV_ROOT}/plugins/pyenv-which-ext"
 }
 
 _zsh_pyenv_load() {
